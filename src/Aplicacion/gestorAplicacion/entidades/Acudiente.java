b247cb31a36a75d9bc9299984e550a0a241647a8
Jtoscanoa <jtoscanoa.personal@gmail.com>
2025-01-14 23:50:43 -0500

    Update Acudiente.java
    Constructores, adición de un número, setters y getters

--- src/Aplicacion/gestorAplicacion/entidades/Acudiente.java
+++ src/Aplicacion/gestorAplicacion/entidades/Acudiente.java
@@ -2,9 +2,29 @@ package gestorAplicacion.entidades;
 
 public class Acudiente extends Persona {
     public Joven protegido;
+    public Long telefono;
 
-    public
+    //Constructor de la clase Acudiente
+    public Acudiente(String nombre, String apellido, int edad, Joven protegido, Long telefono) {
+        super(nombre, apellido, edad);
+        this.protegido = protegido;
+        this.telefono = telefono;
+    }
+
+    //Setters y getters
     public String getRol() {
         return "Acudiente";
+    }
+    public Joven getProtegido() {
+        return protegido;
+    }
+    public Long getTelefono() {
+        return telefono;
+    }
+    public void setProtegido(Joven protegido) {
+        this.protegido = protegido;
+    }
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
     }
 }
