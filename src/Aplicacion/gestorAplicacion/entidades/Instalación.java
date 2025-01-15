03d38b1398c3918340421916d896591a9f3f4295
Jtoscanoa <jtoscanoa.personal@gmail.com>
2025-01-14 22:21:29 -0500

    Creación de la clase toldo y cambios en Instalación
    Se creó una clase toldo, 2 atributos, y la adición de todos los setters y getters

--- src/Aplicacion/gestorAplicacion/entidades/Instalación.java
+++ src/Aplicacion/gestorAplicacion/entidades/Instalación.java
@@ -4,4 +4,37 @@ public class Instalación {
     public int idInstalacion;
     public String deporte;
     public int precioHora;
+    public int profundidad;
+    public Toldo toldo;
+
+    public void setIdInstalacion(int idInstalacion) {
+        this.idInstalacion = idInstalacion;
+    }
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+    public void setPrecioHora(int precioHora) {
+        this.precioHora = precioHora;
+    }
+    public void setProfundidad(int profundidad) {
+        this.profundidad = profundidad;
+    }
+    public void setToldo(Toldo toldo) {
+        this.toldo = toldo;
+    }
+    public int getIdInstalacion() {
+        return idInstalacion;
+    }
+    public String getDeporte() {
+        return deporte;
+    }
+    public int getPrecioHora() {
+        return precioHora;
+    }
+    public int getProfundidad() {
+        return profundidad;
+    }
+    public Toldo getToldo() {
+        return toldo;
+    }
 }
