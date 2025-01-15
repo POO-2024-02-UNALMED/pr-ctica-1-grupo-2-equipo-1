2e28d9a8fd3dc4eeea09d5306aaffa3ceeadd804
Jtoscanoa <jtoscanoa.personal@gmail.com>
2025-01-15 01:01:27 -0500

    Adicion y modificación de clases
    Creación de atributos, constructor y métodos en entrenador, Trabajador Joven y RegistrosEntrenamiento,.

---
+++ src/Aplicacion/gestorAplicacion/entidades/Entrenador.java
@@ -0,0 +1,33 @@
+package gestorAplicacion.entidades;
+
+public class Entrenador extends Trabajador {
+
+    private String deporte;
+    private Instalacion instalacionAsignada;
+
+    // Constructor de la clase Entrenador
+    public Entrenador(String nombre, String apellido, int edad, int salarioHora, String deporte, Instalacion instalacionAsignada) {
+        super(nombre, apellido, edad, salarioHora); // Llamada al constructor de Trabajador
+        this.deporte = deporte;
+        this.instalacionAsignada = instalacionAsignada;
+    }
+
+    // Getters y Setters
+    public String getDeporte() {
+        return deporte;
+    }
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+    public Instalacion getInstalacionAsignada() {
+        return instalacionAsignada;
+    }
+    public void setInstalacionAsignada(Instalacion instalacionAsignada) {
+        this.instalacionAsignada = instalacionAsignada;
+    }
+
+    @Override
+    public String getTrabajo() {
+        return "Entrenador";
+    }
+}
--- src/Aplicacion/gestorAplicacion/entidades/Joven.java
+++ src/Aplicacion/gestorAplicacion/entidades/Joven.java
@@ -1,9 +1,47 @@
 package gestorAplicacion.entidades;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import gestorAplicacion.servicios.RegistrosEntrenamiento;
+
 public class Joven extends Persona {
     public int experienciaJoven;
     public Acudiente acudiente;
     public String eps;
-    public List<Registro> registros
+    public List<gestorAplicacion.servicios.RegistrosEntrenamiento> registrosEntrenamientos;
+
+    // Constructor de la clase Joven
+    public Joven(String nombre, String apellido, int edad, int experiencia, String eps, Acudiente acudiente) {
+        super(nombre, apellido, edad);
+        this.experienciaJoven = experiencia;
+        this.eps = eps;
+        this.acudiente = acudiente;
+        this.registrosEntrenamientos = new ArrayList<>();  // Inicialización de la lista de registros
+    }
+
+    // Setters y getters
+    public String getEps() {
+        return eps;
+    }
+
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
+
+    public List<Registro> getRegistros() {
+        return registrosEntrenamientos;
+    }
+
+    public void setRegistros(List<Registro> registrosEntrenamientos) {
+        this.registrosEntrenamientos = registrosEntrenamientos;
+    }
+
+    public void agregarRegistro(Registro registrosEntrenamiento) {
+        registrosEntrenamientos.add(registrosEntrenamiento);
+    }
+
+    public String getRol() {
+        return "Joven";
+    }
 }
