ef912cd355a8b7c80e6d8bb82afd5c92028ab1ce
Jose Mauricio Toscano Aguas <jtoscanoa.personal@gmail.com>
2025-01-27 21:09:45 -0500

    Update Trabajador.java
    Mejoras de estética

--- src/Aplicacion/gestorAplicacion/entidades/Trabajador.java
+++ src/Aplicacion/gestorAplicacion/entidades/Trabajador.java
@@ -1,20 +1,34 @@
 package gestorAplicacion.entidades;
 
 public class Trabajador extends Persona {
+    //Atributos----------------------------------------------------------
+
     String Rol;
     boolean ocupado = false;
 
+    //-------------------------------------------------------------------
+
+    //Constructores------------------------------------------------------
+
+    //Constructor por defecto.
     public Trabajador() {}
+    //Constructor Completo.
     public Trabajador(String nombre, String apellido, String rol, int edad) {
         super(nombre, apellido, edad);
         this.Rol = rol;
     }
 
+    //-------------------------------------------------------------------
+
+    //Métodos------------------------------------------------------------
+
+    //Método abstracto.
     @Override
     public String getRol() {
         return this.Rol;
     }
 
+    //Setters y Getters
     public boolean isOcupado() {
         return ocupado;
     }
@@ -22,6 +36,6 @@ public class Trabajador extends Persona {
     public void setOcupado(boolean ocupado) {
         this.ocupado = ocupado;
     }
-
-
+    
+    //-------------------------------------------------------------------
 }
