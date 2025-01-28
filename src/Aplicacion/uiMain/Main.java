f17a5b019ce811e8a40ea3d848d51b9dfbc6c109
Jose Mauricio Toscano Aguas <jtoscanoa.personal@gmail.com>
2025-01-28 01:17:06 -0500

    Revert "Reapply "Debug del "revert del "debug""""
    This reverts commit 9b0700514bf0354c1251caef01aaa7210d649f0f.

--- src/Aplicacion/uiMain/Main.java
+++ src/Aplicacion/uiMain/Main.java
@@ -96,6 +96,8 @@ public class Main {
                     System.out.println("Opción inválida. Por favor, seleccione una opción del 1 al 7.");
             }
         }
+
+        scanner.close();
     }
 
     //////////////////////////MENU RESERVAS//////////////////////////////////////////
